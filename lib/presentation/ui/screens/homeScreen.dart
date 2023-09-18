1f26327531332d8115cec5adfa6feeda9ed17ea3
nazmulhaquenahin <nahin.cdr@gmail.com>
2023-09-18 19:54:06 +0600

    carousel slider added

--- lib/presentation/ui/screens/homeScreen.dart
+++ lib/presentation/ui/screens/homeScreen.dart
@@ -1,6 +1,5 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:ecommerce/presentation/ui/utility/assetManager.dart';
-import 'package:ecommerce/presentation/ui/utility/colorManager.dart';
+import 'package:ecommerce/presentation/ui/widgets/home/homeSlider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -14,58 +13,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-  final ValueNotifier<int> _selectedSlider = ValueNotifier(0);
-  List<Widget>dotList = [
-    Container(
-      margin: const EdgeInsets.symmetric(horizontal: 4),
-      width: 12,
-      height: 12,
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(10)
-      ),
-    ),
-    Container(
-      margin: const EdgeInsets.symmetric(horizontal: 4),
-      width: 12,
-      height: 12,
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(10)
-      ),
-    ),
-    Container(
-      margin: const EdgeInsets.symmetric(horizontal: 4),
-      width: 12,
-      height: 12,
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(10)
-      ),
-    ),
-    Container(
-      margin: const EdgeInsets.symmetric(horizontal: 4),
-      width: 12,
-      height: 12,
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(10)
-      ),
-    ),
-    Container(
-      margin: const EdgeInsets.symmetric(horizontal: 4),
-      width: 12,
-      height: 12,
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(10)
-      ),
-    )
-  ];
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -110,83 +57,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               const SizedBox(height: 20),
-              CarouselSlider(
-                options: CarouselOptions(
-                    height: 180.0,
-                    autoPlay: true,
-                    autoPlayInterval: const Duration(seconds: 3),
-                    onPageChanged: (int page,_){
-                      _selectedSlider.value = page;
-                    }
-                ),
-                items: [1,2,3,4,5].map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                          decoration: const BoxDecoration(
-                              color: Colors.amber
-                          ),
-                          child: Text('text $i', style: const TextStyle(fontSize: 16.0),)
-                      );
-                    },
-                  );
-                }).toList(),
-              ),
-              const SizedBox(height: 16),
-              ValueListenableBuilder(
-                  valueListenable: _selectedSlider,
-                  builder: (context,value,_){
-                    if(value>0){
-                      dotList[value-1] = Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 4),
-                        width: 12,
-                        height: 12,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(10),
-                            color:null
-                        ),
-                      );
-                      dotList[value] = Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 4),
-                        width: 12,
-                        height: 12,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(10),
-                            color: ColorsManager.appPrimaryColor
-                        ),
-                      );
-                    }else{
-                      dotList[4] = Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 4),
-                        width: 12,
-                        height: 12,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(10),
-                            color: null
-                        ),
-                      );
-                      dotList[value] = Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 4),
-                        width: 12,
-                        height: 12,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(10),
-                            color: ColorsManager.appPrimaryColor
-                        ),
-                      );
-                    }
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: dotList,
-                    );
-                  }
-              )
+              const HomeSlider()
             ],
           ),
         )
@@ -194,3 +65,5 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+
---
+++ lib/presentation/ui/widgets/home/homeSlider.dart
@@ -0,0 +1,146 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+import '../../utility/colorManager.dart';
+
+class HomeSlider extends StatefulWidget {
+  const HomeSlider({super.key});
+
+  @override
+  State<HomeSlider> createState() => _HomeSliderState();
+}
+
+class _HomeSliderState extends State<HomeSlider> {
+  final ValueNotifier<int> _selectedSlider = ValueNotifier(0);
+  List<Widget>dotList = [
+    Container(
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+      width: 12,
+      height: 12,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10)
+      ),
+    ),
+    Container(
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+      width: 12,
+      height: 12,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10)
+      ),
+    ),
+    Container(
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+      width: 12,
+      height: 12,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10)
+      ),
+    ),
+    Container(
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+      width: 12,
+      height: 12,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10)
+      ),
+    ),
+    Container(
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+      width: 12,
+      height: 12,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10)
+      ),
+    )
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+              height: 180.0,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 3),
+              onPageChanged: (int page,_){
+                _selectedSlider.value = page;
+              }
+          ),
+          items: [1,2,3,4,5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: const BoxDecoration(
+                        color: Colors.amber
+                    ),
+                    child: Text('text $i', style: const TextStyle(fontSize: 16.0),)
+                );
+              },
+            );
+          }).toList(),
+        ),
+        const SizedBox(height: 16),
+        ValueListenableBuilder(
+            valueListenable: _selectedSlider,
+            builder: (context,value,_){
+              if(value>0){
+                dotList[value-1] = Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                      color:null
+                  ),
+                );
+                dotList[value] = Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                      color: ColorsManager.appPrimaryColor
+                  ),
+                );
+              }else{
+                dotList[4] = Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                      color: null
+                  ),
+                );
+                dotList[value] = Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                      color: ColorsManager.appPrimaryColor
+                  ),
+                );
+              }
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: dotList,
+              );
+            }
+        )
+      ],
+    );
+  }
+}
